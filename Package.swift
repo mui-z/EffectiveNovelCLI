dc1130aee34e56e572591197a92f8a17e1357736
mui-z <93278577+mui-z@users.noreply.github.com>
2022-12-13 02:07:24 +0900

    update: update lib
    update: update resolved

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .package(url: "https://github.com/onevcat/Rainbow", from: "4.0.1"),
         .package(url: "https://github.com/kylef/PathKit", from: "1.0.0"),
         .package(url: "https://github.com/jakeheis/SwiftCLI", from: "6.0.3"),
-        .package(url: "https://github.com/mui-z/effective-novel-core", from: "0.0.3")
+        .package(url: "https://github.com/mui-z/effective-novel-core", from: "1.1.2")
     ],
     targets: [
         .executableTarget(name: "EffectiveNovelCLI", dependencies: [
@@ -21,7 +21,7 @@ let package = Package(
             "SwiftCLI",
             "PathKit",
             "Rainbow",
-            .product(name: "EffectiveNovelCore", package: "effective-novel-core")
+            .product(name: "EffectiveNovelCore", package: "effective-novel-core"),
         ]),
         .target(name: "EffectiveNovelCLIKit", dependencies: [
             "PathKit",
